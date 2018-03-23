8913080c8ae6bcc16de002e09e2905d3ab9f38c1
MewX <imewx@qq.com>
2018-03-23 14:08:11 +1030

    init projects

---
+++ Analyser/src/main/java/Analyser.java
@@ -0,0 +1,5 @@
+public class Analyser {
+    public static void main(String[] args) {
+        System.out.println("sample output");
+    }
+}
---
+++ Collector/src/main/java/Collector.java
@@ -0,0 +1,5 @@
+public class Collector {
+    public static void main(String[] args) {
+        System.out.println("sample output");
+    }
+}

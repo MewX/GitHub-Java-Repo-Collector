67672abd5bf383d94dd8a213a617790d38527c05
MewX <imewx@qq.com>
2018-03-26 13:59:33 +1030

    Saved for home editing

--- Collector/src/main/java/Constants.java
+++ Collector/src/main/java/Constants.java
@@ -3,5 +3,7 @@ public class Constants {
     static final String HEADER_X_RATELIMIT_REMAINING = "X-RateLimit-Remaining"; // 4999
     static final String HEADER_X_RATELIMIT_RESET = "X-RateLimit-Reset"; // 1521981352
 
-    static final String APP_ID_FOR_QUERY = "&client_id=Iv1.ad6b73b7b61c26f3&client_secret=9ac009128eb89e8c2b0f9d39fddd0378d3dfbdc0";
+    static final String APP_CLIENT_ID = "Iv1.ad6b73b7b61c26f3";
+    static final String APP_CLIENT_SECRET = "9ac009128eb89e8c2b0f9d39fddd0378d3dfbdc0";
+    static final String APP_ID_FOR_QUERY = "&client_id=" + APP_CLIENT_ID + "&client_secret=" + APP_CLIENT_SECRET;
 }

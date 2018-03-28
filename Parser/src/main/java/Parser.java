20ae27e88ec1a5a7d5b2ec07a1a72b88cc3ed906
nick lee <nicklee1006@gmail.com>
2018-03-29 00:19:41 +1030

    modify pom parser

--- Parser/src/main/java/Parser.java
+++ Parser/src/main/java/Parser.java
@@ -1,41 +1,23 @@
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.IOException;
 
 public class Parser {
     public static void main(String[] args) {
-        try {
-            // TODO modify file path
-            File testFile = new File("test/pom.xml");
+        // TODO modify file path
+        File testFile = new File("test/pom.xml");
+        File testGradle = new File("test/test2-build.gradle");
 
-            // parser for pom.xml
-            PomParser pomParser = new PomParser();
+        // parser for pom.xml
+        PomParser pomParser = new PomParser();
+        // parser for build.gradle
+        GradleParser gradleParser = new GradleParser();
 
-            // read in xml file
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(testFile);
 
-            document.getDocumentElement().normalize();
-
-            // call pom parser
-            if (testFile.getName().contains("pom.xml")) {
-                pomParser.parsePomFile(document);
-            }
-
-            // call gradle parser
-
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        // call pom parser
+        if (testGradle.getName().contains("pom.xml")) {
+            pomParser.parsePomFile(testFile);
+        // call gradle parser
+        } else if (testGradle.getName().contains("build.gradle")) {
+            gradleParser.parseGradleFile(testGradle);
         }
     }
 }
--- Parser/src/main/java/PomParser.java
+++ Parser/src/main/java/PomParser.java
@@ -1,20 +1,42 @@
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 
 public class PomParser {
-    public void parsePomFile(Document document) {
-        NodeList dependencies = document.getElementsByTagName("dependency");
+    public void parsePomFile(File file) {
+        try {
+            // read in xml file
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder;
 
-        for (int i = 0; i < dependencies.getLength(); i++) {
-            Element dependency = (Element) dependencies.item(i);
+            builder = factory.newDocumentBuilder();
+            Document document = builder.parse(file);
+            document.getDocumentElement().normalize();
 
-            System.out.println("GroupId: " + dependency.getElementsByTagName("groupId").item(0).getTextContent());
-            System.out.println("artifactId: " + dependency.getElementsByTagName("artifactId").item(0).getTextContent());
-            System.out.println("version: " + dependency.getElementsByTagName("version").item(0).getTextContent());
-            System.out.println("\n");
+            NodeList dependencies = document.getElementsByTagName("dependency");
 
-            // TODO output results
+            for (int i = 0; i < dependencies.getLength(); i++) {
+                Element dependency = (Element) dependencies.item(i);
+
+                System.out.println("GroupId: " + dependency.getElementsByTagName("groupId").item(0).getTextContent());
+                System.out.println("artifactId: " + dependency.getElementsByTagName("artifactId").item(0).getTextContent());
+                System.out.println("version: " + dependency.getElementsByTagName("version").item(0).getTextContent());
+                System.out.print("\n");
+
+                // TODO output results
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }

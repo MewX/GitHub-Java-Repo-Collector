5f4512f11c4a272d220720509f0bdfcda192005f
nick lee <nicklee1006@gmail.com>
2018-04-06 16:49:27 +0930

    skip hidden folder when process

--- Parser/src/main/java/Parser.java
+++ Parser/src/main/java/Parser.java
@@ -1,4 +1,6 @@
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
 
 import java.io.File;
 import java.util.Collection;
@@ -13,8 +15,7 @@ public class Parser {
         // TODO modify file path
         // absolute path of project folder (root folder)
         File inputFile = new File(args[0]);
-//        File inputFile = new File("/Users/nick/Downloads/incubator-dubbo-master");
-        String[] extensions = {"xml", "gradle"};
+//        File inputFile = new File("/Users/nick/Documents/Github/MyAustralia2");
 
         // project name based on file path
         String projectName = inputFile.getName();
@@ -30,7 +31,15 @@ public class Parser {
         System.out.println("Processing project: " + projectName);
 
         // get all "pom.xml" and "build.gradle" from sub-folder
-        Collection<File> files = FileUtils.listFiles(inputFile, extensions, true);
+        Collection<File> files = FileUtils.listFiles(inputFile, new IOFileFilter() {
+            public boolean accept(File file) {
+                return file.getName().equals("pom.xml") || file.getName().equals("build.gradle");
+            }
+
+            public boolean accept(File file, String s) {
+                return true;
+            }
+        }, HiddenFileFilter.VISIBLE);
 
         for (File file : files) {
             if (file.getName().equals("pom.xml")) {
